e4211c33b5951b911ef082202b9322d8c70f107d
Yulij Andreevich Lesov <yalesov@gmail.com>
2016-06-22 20:49:29 +0800

    a simple infinite-loop test script, for help testing the random nature of password output

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+default_ext='js'
+default_log='error.log'
+
+ext=${1-$default_ext}
+log=${2-$default_log}
+
+i=0
+while [ 1 ]; do
+  i=$((i+1))
+  echo $i
+  count=`./test.js ./password.$ext | tee $log | ag 'failed' | wc -l`
+  [ $count -gt 0 ] && break
+done
